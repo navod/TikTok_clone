ee02941ea1d5d49124296b2be1dc37246777b112
Navod21 <navod@commercialtp.com>
2022-08-17 20:06:23 +0530

    created sanity schemas

---
+++ sanitybackend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      type: "string",
+      title: "User Name",
+    },
+    {
+      name: "image",
+      type: "string",
+      title: "Image",
+    },
+  ],
+};
